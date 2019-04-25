3e9e2e919fd4d8513d550eb1e7b1e2f42059988e
William S. Rodriguez <williamsrz@outlook.com>
2019-04-25 14:59:39 -0300

    🚧🚨⚠️ This commit is a lie

--- src/app/Evidences/Evidences/Evidences/ViewModel/MainViewModel.cs
+++ src/app/Evidences/Evidences/Evidences/ViewModel/MainViewModel.cs
@@ -3,59 +3,34 @@ using Evidences.Services;
 using Xamarin.Forms;
 using System.Linq;
 using Evidences.Models;
+using Prism.Navigation;
+using Prism.Commands;
 
 namespace Evidences.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
-        protected ISignalRService SignalRService { get; }
         protected IYoutubeSearchService YoutubeSearchService { get; }
-        protected IUserService UserService { get; }
         protected ISongService SongService { get; }
 
-        public MainViewModel(
-                IStateService stateService,
-                ISignalRService signaRService,
-                IYoutubeSearchService youtubeSearchService,
-                IUserService userService,
-                ISongService songService
-            ) : base(stateService)
+        public MainViewModel(IStateService stateService,
+            IUserService userService,
+            ISignalRService signaRService,
+            INavigationService navigationService,
+            IYoutubeSearchService youtubeSearchService,
+            ISongService songService) :
+             base(stateService, userService, signaRService, navigationService)
         {
-            SignalRService = signaRService;
             YoutubeSearchService = youtubeSearchService;
-            UserService = userService;
             SongService = songService;
 
-            CreateUser = new Command(CreateUserExecute);
-            SearchYoutube = new Command(SearchYoutubeExecute);
+            SearchYoutube = new DelegateCommand(SearchYoutubeExecute);
 
             RegisterSignalREvents();
         }
 
-        public string UserName { get; set; }
-
         public string YoutubeSearchQuery { get; set; }
-
-        public ICommand CreateUser { get; }
-
-        public ICommand SearchYoutube { get; }
-
-        public User CurentUser => UserService.Get();
-
-        private async void CreateUserExecute()
-        {
-            try
-            {
-                if (CurentUser == null)
-                {
-                    await UserService.Add(UserName);
-                }
-                await SignalRService.Connect();
-            }
-            catch (System.Exception ex)
-            {
-            }
-        }
+        public DelegateCommand SearchYoutube { get; }
 
         private async void SearchYoutubeExecute()
         {
@@ -89,32 +64,5 @@ namespace Evidences.ViewModel
             }
         }
 
-        private void RegisterSignalREvents()
-        {
-            SignalRService.OnRemoveSong += SignalRService_OnRemoveSong;
-            SignalRService.OnSongAdded += SignalRService_OnSongAdded;
-            SignalRService.OnSongFinished += SignalRService_OnSongFinished;
-            SignalRService.OnSongStarted += SignalRService_OnSongStarted;
-        }
-
-        private void SignalRService_OnSongStarted(object sender, Models.CurrentSong e)
-        {
-            
-        }
-
-        private void SignalRService_OnSongFinished(object sender, Models.CurrentSong e)
-        {
-            
-        }
-
-        private void SignalRService_OnSongAdded(object sender, Models.Song e)
-        {
-            
-        }
-
-        private void SignalRService_OnRemoveSong(object sender, Models.SongToRemove e)
-        {
-            
-        }
     }
 }
---
+++ src/app/Evidences/Evidences/Evidences/ViewModel/OnboardingViewModel.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoginViewModel.cs" company="ArcTouch LLC">
+//   Copyright 2019 ArcTouch LLC.
+//   All rights reserved.
+//
+//   This file, its contents, concepts, methods, behavior, and operation
+//   (collectively the "Software") are protected by trade secret, patent,
+//   and copyright laws. The use of the Software is governed by a license
+//   agreement. Disclosure of the Software to third parties, in any form,
+//   in whole or in part, is expressly prohibited except as authorized by
+//   the license agreement.
+// </copyright>
+// <summary>
+//   Defines the LoginViewModel type.
+// </summary>
+//  --------------------------------------------------------------------------------------------------------------------
+using System;
+using Evidences.Services;
+using Prism.Commands;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace Evidences.ViewModel
+{
+    public class OnboardingViewModel : BaseViewModel
+    {
+        public OnboardingViewModel(IStateService stateService,
+            IUserService userService,
+            ISignalRService signaRService,
+            INavigationService navigationService)
+            : base(stateService, userService, signaRService, navigationService)
+        {
+            CreateUser = new DelegateCommand(CreateUserExecute);
+        }
+
+        public string UserName { get; set; }
+        public DelegateCommand CreateUser { get; }
+
+        private async void CreateUserExecute()
+        {
+            try
+            {
+                if (CurentUser == null)
+                    await UserService.Add(UserName);
+
+                await SignalRService.Connect();
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
+

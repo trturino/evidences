6551b995214f7fcdb276b26379f58a742e78daf5
Rafael Turino <rafael.turino@arctouch.com>
2019-04-26 15:32:59 -0300

    Added reactions

--- src/app/Evidences/Evidences/Evidences/ViewModel/MainViewModel.cs
+++ src/app/Evidences/Evidences/Evidences/ViewModel/MainViewModel.cs
@@ -11,6 +11,7 @@ namespace Evidences.ViewModel
     public class MainViewModel : BaseViewModel
     {
         protected IYoutubeSearchService YoutubeSearchService { get; }
+        protected IReactionService ReactionService { get; }
         protected ISongService SongService { get; }
 
         public MainViewModel(IStateService stateService,
@@ -18,19 +19,25 @@ namespace Evidences.ViewModel
             ISignalRService signaRService,
             INavigationService navigationService,
             IYoutubeSearchService youtubeSearchService,
+            IReactionService reactionService,
             ISongService songService) :
              base(stateService, userService, signaRService, navigationService)
         {
             YoutubeSearchService = youtubeSearchService;
+            ReactionService = reactionService;
             SongService = songService;
 
             SearchYoutube = new DelegateCommand(SearchYoutubeExecute);
+            SendReaction = new DelegateCommand(SendReactionExecute);
 
             RegisterSignalREvents();
         }
 
         public string YoutubeSearchQuery { get; set; }
+        public string Reaction { get; set; }
         public DelegateCommand SearchYoutube { get; }
+
+        public DelegateCommand SendReaction { get; }
 
         private async void SearchYoutubeExecute()
         {
@@ -64,5 +71,16 @@ namespace Evidences.ViewModel
             }
         }
 
+        private async void SendReactionExecute()
+        {
+            try
+            {
+                await ReactionService.SendReaction(Reaction);
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+        }
     }
 }

daf46054e596f2b888c264f009e4d7ea6de546d3
willbuildapps <williamsrz@outlook.com>
2019-04-30 13:37:12 -0300

    💄 added effect

---
+++ src/app/Evidences/Evidences/Evidences.iOS/Effects/ShadowPlatformEffect.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShadowPlatformEffect.cs" company="ArcTouch LLC">
+//   Copyright 2019 ArcTouch LLC.
+//   All rights reserved.
+//
+//   This file, its contents, concepts, methods, behavior, and operation
+//   (collectively the "Software") are protected by trade secret, patent,
+//   and copyright laws. The use of the Software is governed by a license
+//   agreement. Disclosure of the Software to third parties, in any form,
+//   in whole or in part, is expressly prohibited except as authorized by
+//   the license agreement.
+// </copyright>
+// <summary>
+//   Defines the ShadowPlatformEffect type.
+// </summary>
+//  --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Linq;
+using CoreGraphics;
+using Evidences.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("Evidences.ShadowEffect")]
+[assembly: ExportEffect(typeof(ShadowEffect), "ShadowEffect")]
+namespace Evidences.iOS.Effects
+{
+    public class ShadowPlatformEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var effect = (ShadowEffect)Element.Effects.FirstOrDefault(e => e is ShadowEffect);
+
+                if (effect != null)
+                {
+                    Container.Layer.CornerRadius = effect.BlurRadius;
+                    Container.Layer.ShadowColor = effect.ShadowColor.ToCGColor();
+                    Container.Layer.ShadowOffset = new CGSize(effect.HorizontalLength, effect.VerticalLength);
+                    Container.Layer.ShadowOpacity = effect.ShadowOpacity;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ShadowPlatformEffect 💩 ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}

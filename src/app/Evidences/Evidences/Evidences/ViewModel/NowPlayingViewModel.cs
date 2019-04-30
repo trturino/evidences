4971279f4fdc47b12aedf12b124b4b78c7e7402c
willbuildapps <williamsrz@outlook.com>
2019-04-30 16:51:29 -0300

    🚧 view bootstrap

---
+++ src/app/Evidences/Evidences/Evidences/ViewModel/NowPlayingViewModel.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NowPlayingViewModel.cs" company="ArcTouch LLC">
+//   Copyright 2019 ArcTouch LLC.
+//   All rights reserved.
+//
+//   This file, its contents, concepts, methods, behavior, and operation
+//   (collectively the "Software") are protected by trade secret, patent,
+//   and copyright laws. The use of the Software is governed by a license
+//   agreement. Disclosure of the Software to third parties, in any form,
+//   in whole or in part, is expressly prohibited except as authorized by
+//   the license agreement.
+// </copyright>
+// <summary>
+//   Defines the NowPlayingViewModel type.
+// </summary>
+//  --------------------------------------------------------------------------------------------------------------------
+using System;
+using Evidences.Services;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace Evidences.ViewModel
+{
+    public class NowPlayingViewModel : BaseViewModel
+    {
+        public NowPlayingViewModel(IStateService stateService,
+            IUserService userService,
+            ISignalRService signaRService,
+            INavigationService navigationService)
+            : base(stateService, userService, signaRService, navigationService)
+        {
+
+        }
+    }
+}
+
---
+++ src/app/Evidences/Evidences/Evidences/Views/NowPlayingPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NowPlayingPage.xaml.cs" company="ArcTouch LLC">
+//   Copyright 2019 ArcTouch LLC.
+//   All rights reserved.
+//
+//   This file, its contents, concepts, methods, behavior, and operation
+//   (collectively the "Software") are protected by trade secret, patent,
+//   and copyright laws. The use of the Software is governed by a license
+//   agreement. Disclosure of the Software to third parties, in any form,
+//   in whole or in part, is expressly prohibited except as authorized by
+//   the license agreement.
+// </copyright>
+// <summary>
+//   Defines the NowPlayingPage.xaml type.
+// </summary>
+//  --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Evidences.Views
+{
+    public partial class NowPlayingPage : ContentPage
+    {
+        public NowPlayingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
